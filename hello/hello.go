2096f26c87f3e0d3ab16374d20e7b5acebc2f88d
Khalil Saboor <khalilsaboor1@gmail.com>
2024-02-19 19:14:01 -0500

    handling errors

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	fmt.Println("Hello, World!")
-	message := greetings.Hello("khalil")
+
+	message, err := greetings.Hello("")
+	// Request a greeting message and print out a quote.
+	// if an error was returned, print it to the console and
+	// exit
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 	fmt.Println(quote.Go())
 }
